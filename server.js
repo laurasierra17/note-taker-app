3f6c440fa246cd10e39da6aa6ed57d7d4f052d00
laurasierra17 <laura.sierra17@gmail.com>
2022-07-25 18:42:13 -0400

    static html middleware

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ const PORT = 3001;
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 app.listen(PORT, () => console.log("Listening on port 3001..."));
