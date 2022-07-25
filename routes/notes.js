059b208dfa17713e2ff741cfcbeb5f629d250b42
laurasierra17 <laura.sierra17@gmail.com>
2022-07-25 19:31:11 -0400

    retrieve saved notes from json file and render on page

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,12 @@
 const notes = require('express').Router();
 
-//GET /api/notes should read the db.json file and return all saved notes as JSON.
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
 notes.get('/', (req, res) => {
-    
+    readFromFile('../db/db.json').then((data) =>
+    res.json(JSON.parse(data))
+  );
 })
+
 // POST /api/notes should receive a new note to save on the request body, add 
 // it to the db.json file, and then return the new note to the client. 
 // You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
