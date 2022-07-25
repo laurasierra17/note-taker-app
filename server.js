0c1d41a5d680992473492089e00bf65b1ca89ab5
laurasierra17 <laura.sierra17@gmail.com>
2022-07-25 18:40:37 -0400

    middleware added

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.listen(PORT, () => console.log("Listening on port 3001..."));
