8d8a8bb65fa6c984fd76d5cf31185f4e62cd2069
laurasierra17 <laura.sierra17@gmail.com>
2022-07-25 19:47:31 -0400

    save new note and add it to list of previous notes

--- routes/notes.js
+++ routes/notes.js
@@ -5,8 +5,8 @@ const { v4: uuidv4 } = require('uuid');
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) =>
-    res.json(JSON.parse(data))
-  );
+        res.json(JSON.parse(data))
+    );
 })
 
 // POST /api/notes should receive a new note to save on the request body, add 
@@ -14,10 +14,28 @@ notes.get('/', (req, res) => {
 // You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 notes.post('/', (req, res) => {
     // Destructuring assignment for the items in req.body
-  const { title, text } = req.body;
+    const { title, text } = req.body;
 
-  
-  }
+    // If all the required properties are present
+    if (title && text) {
+        // Variable for the object we will save
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json('Error in posting note');
+    }
 })
 
 module.exports = notes;
