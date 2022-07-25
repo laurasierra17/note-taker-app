1625deb1861bf4b6e42317057307e13fb522f7e3
laurasierra17 <laura.sierra17@gmail.com>
2022-07-25 18:51:36 -0400

    require path library to send notes.html to /notes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3001;
 
@@ -7,8 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-
 // Return notes page
-app.get('/notes', (req, res) => res.sendFile('./public/notes.html'));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
 app.listen(PORT, () => console.log("Listening on port 3001..."));
