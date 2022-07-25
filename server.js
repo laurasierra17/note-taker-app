1d8a5c40e7c6f74bcfd98ce4649a3e2b99029c2f
laurasierra17 <laura.sierra17@gmail.com>
2022-07-25 18:48:34 -0400

    send notes page to /notes

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+
+// Return notes page
+app.get('/notes', (req, res) => res.sendFile('./public/notes.html'));
+
 app.listen(PORT, () => console.log("Listening on port 3001..."));
