cfddb832101ed1155d8329b66261dbfedd998fd4
laurasierra17 <laura.sierra17@gmail.com>
2022-07-25 19:46:40 -0400

    grab text and title from user's typed new note

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,10 @@
 const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/utils');
+const { v4: uuidv4 } = require('uuid');
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 notes.get('/', (req, res) => {
-    readFromFile('../db/db.json').then((data) =>
+    readFromFile('./db/db.json').then((data) =>
     res.json(JSON.parse(data))
   );
 })
@@ -10,3 +12,12 @@ notes.get('/', (req, res) => {
 // POST /api/notes should receive a new note to save on the request body, add 
 // it to the db.json file, and then return the new note to the client. 
 // You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+notes.post('/', (req, res) => {
+    // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  
+  }
+})
+
+module.exports = notes;
