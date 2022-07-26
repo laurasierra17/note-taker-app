6d398aacfc5db4517fc80b332b5a738ecfad6b87
laurasierra17 <laura.sierra17@gmail.com>
2022-07-25 20:37:04 -0400

    delete method added to notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,5 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/utils');
+const { readFromFile, readAndAppend, readAndDelete } = require('../helpers/utils');
 const { v4: uuidv4 } = require('uuid');
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
@@ -43,10 +43,20 @@ notes.delete('/:id', (req, res) => {
     // Assignment for the items in req.params
     const noteId = req.params.id;
 
+    // If the id is present
+    if (noteId) {
+        readAndDelete(noteId, './db/db.json');
 
-    // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, 
-    // you'll need to read all notes from the db.json file, 
-    // remove the note with the given id property, and then rewrite the notes to the db.json file.
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        // Sned response back to the client
+        res.json(response);
+    } else {
+        res.json('Error in deleting note');
+    }
 })
 
 module.exports = notes;
