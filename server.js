ec18af323f83a1f02793c06bc0bc9365c50c1b06
laurasierra17 <laura.sierra17@gmail.com>
2022-07-25 20:57:29 -0400

    fix order of routes

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ app.use('/api', api);
 // Landing page is set to index.html found in the public directory
 app.use(express.static('public'));
 
+// GET route for notes page
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+
 // Wildcard route to direct users to a the landing page
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
-
-// GET route for notes page
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
 app.listen(PORT, () => console.log("Listening on port 3001..."));
